018ee32e4a5f08aa58bca360b2730e90621d7b30
night-crawler <lilo.panic@gmail.com>
2025-06-29 23:41:46 +0100

    wip

--- configuration.nix
+++ configuration.nix
@@ -1,92 +1,85 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  zfsCompatibleKernelPackages =
-    lib.filterAttrs (
-      name: kp:
-      (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-      && (builtins.tryEval kp).success
-      && (!kp.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-    ) pkgs.linuxKernel.packages;
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kp:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kp).success
+    && (!kp.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
 
-  latestKernelPackage =
-    lib.last (lib.sort (a: b: lib.versionOlder a.kernel.version b.kernel.version)
-                     (builtins.attrValues zfsCompatibleKernelPackages));
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: lib.versionOlder a.kernel.version b.kernel.version) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   boot.kernelPackages = latestKernelPackage;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   nixpkgs.config.allowUnfree = true;
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
-  
+
   hardware.graphics.enable = true;
 
-  fileSystems."/" =
-    { device = "penguins/root";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/" = {
+    device = "penguins/root";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
-  fileSystems."/home" =
-    { device = "penguins/home";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/home" = {
+    device = "penguins/home";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
-  fileSystems."/usr" =
-    { device = "penguins/usr";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/usr" = {
+    device = "penguins/usr";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
-  fileSystems."/work" =
-    { device = "penguins/work";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/work" = {
+    device = "penguins/work";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
-  fileSystems."/nix" =
-    { device = "penguins/nix";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/nix" = {
+    device = "penguins/nix";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
-  fileSystems."/etc" =
-    { device = "penguins/etc";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
+  fileSystems."/etc" = {
+    device = "penguins/etc";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 
   services.zfs = {
-    autoScrub.enable = true;               # weekly scrub
-    trim.enable      = true;               # periodic full-pool TRIM
+    autoScrub.enable = true;
+    trim.enable = true;
   };
-  
+
   services.tlp.enable = true;
-  #services.auto-cpufreq.enable = true;
   services.power-profiles-daemon.enable = false;
-  #services.auto-cpufreq.settings = {
-  #  battery = {
-  #    governor = "powersave";
-  #    turbo = "never";
-  #  };
-  #  charger = {
-  #    governor = "performance";
-  #    turbo = "auto";
-  #  };
-  #};
 
   boot.kernelParams = [
     "zfs.zfs_arc_max=12884901888"
@@ -98,8 +91,8 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs = {
-    devNodes    = "/dev/disk/by-id";
-    extraPools  = [ "penguins" ];
+    devNodes = "/dev/disk/by-id";
+    extraPools = [ "penguins" ];
   };
 
   systemd.services.zfs-mount.enable = false;
@@ -113,32 +106,22 @@ in
 
   time.timeZone = "Europe/Dublin";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-#   console = {
-#     font = "Lat2-Terminus16";
-#     keyMap = "us";
-#     useXkbConfig = true; # use xkb.options in tty.
-#   };
+  #   console = {
+  #     font = "Lat2-Terminus16";
+  #     keyMap = "us";
+  #     useXkbConfig = true; # use xkb.options in tty.
+  #   };
 
   services = {
-    #xserver.enable = true;
-    
     xserver = {
-      enable = true;                            # already present
+      enable = true;
       xkb = {
-        # two layouts available system-wide
-        layout  = "us,ru";                      # order: English first, Russian second
+        layout = "us,ru";
         options = "grp:caps_toggle,grp_led:caps";
-        #  grp:caps_toggle → Caps Lock cycles layouts
-        #  grp_led:caps    → Caps LED shows current layout (on = Russian)
       };
     };
-    
+
     libinput.enable = true;
     displayManager.sddm = {
       enable = true;
@@ -167,11 +150,9 @@ in
     "d /work 2775 user users - -"
   ];
 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" ];
     packages = with pkgs; [
       tree
     ];
@@ -188,30 +169,52 @@ in
     enableSSHSupport = true;
   };
 
-  # List services that you want to enable:
-
   services.openssh.enable = true;
-
   services.tailscale.enable = true;
 
   environment.systemPackages = lib.mkMerge [
     (with pkgs; [
-      gajim telegram-desktop
-      ripgrep bat git wget mc curl imhex unzip
-      htop btop sysstat iftop cpufrequtils powertop
-      lm_sensors smartmontools
+      gajim
+      telegram-desktop
+      ripgrep
+      bat
+      git
+      wget
+      mc
+      curl
+      imhex
+      unzip
+      nil
+      nixd
+      htop
+      btop
+      sysstat
+      iftop
+      cpufrequtils
+      powertop
+      lm_sensors
+      smartmontools
       tailscale
-      vlc haruna
+      vlc
+      haruna
       libreoffice-fresh
-      jetbrains-toolbox zed-editor
+      jetbrains-toolbox
+      zed-editor
       thunderbird
     ])
     (with pkgs.kdePackages; [
-      konsole dolphin kate spectacle gwenview okular ark kcalc
-      kolourpaint kdeconnect-kde
+      konsole
+      dolphin
+      kate
+      spectacle
+      gwenview
+      okular
+      ark
+      kcalc
+      kolourpaint
+      kdeconnect-kde
     ])
   ];
 
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
-
--- flake.nix
+++ flake.nix
@@ -3,14 +3,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
   };
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    nixos-hardware,
-    ... }: {
-    # NOTE: 'nixos' is the default hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixos-hardware,
+      ...
+    }:
+    {
+
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        modules = [
+          nixos-hardware.nixosModules.framework-16-7040-amd
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
